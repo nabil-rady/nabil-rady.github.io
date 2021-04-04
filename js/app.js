3559a2a902d1c7e46db695e23f79bac16784208e
My-Name-Is-Nabil <midorady9999@gmail.com>
2021-04-04 22:56:22 +0200

    Added mobile menus

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 const [product,company,overview] = document.querySelectorAll('.header__nav__ul__li');
 const headerNav = document.querySelector('.header__nav__ul');
+const burgerMenu = document.querySelector('.header__nav__burgers__burger__box');
+const burgerMenuCenter = document.querySelector('.header__nav__burgers__burger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuUl = mobileMenu.children[0].children[0];
+const mobileMediaQuery = window.matchMedia('(max-width: 1170px)');
 const clickOutsideMenu = event => !( event.target.matches('.menu') || event.target.parentNode && event.target.parentNode.matches('.menu') ) ;
 const clickOnNavItem = event => event.target.matches('.header__nav__ul__li');
 const showMenu = event =>{
@@ -41,5 +46,39 @@ const hideMenu = event =>{
         }
     }
 };
+
+const toggleMobileMenu = () => {
+    mobileMenu.classList.toggle('visible');
+    burgerMenuCenter.classList.toggle('open');
+};
+const hideMobileMenu = () => {
+    mobileMenu.classList.remove('visible');
+    burgerMenuCenter.classList.remove('open');
+};
+
+const toggleMobileMiniMenu = event => {
+    event = event || window.event;
+    const listElement = event.target;
+    if (listElement.nodeName === 'LI'){
+        const arrowElement = listElement.children[0];
+        const miniMenu = listElement.children[1];
+        listElement.classList.toggle('focused');
+        arrowElement.classList.toggle('rotate');
+        miniMenu.classList.toggle('visible');
+    }
+}
+
 headerNav.addEventListener('click', showMenu);
+burgerMenu.addEventListener('click',toggleMobileMenu);
+mobileMenuUl.addEventListener('click',toggleMobileMiniMenu);
 document.addEventListener('click', hideMenu);
+
+const positionMobileMenu = () =>{
+    mobileMenu.setAttribute('style', `left:calc((100% - ${mobileMenu.clientWidth}px)/2);`);
+};
+document.addEventListener('DOMContentLoaded',positionMobileMenu);
+window.onresize = positionMobileMenu;
+mobileMediaQuery.addListener(query => {
+    if (!query.matches)
+        hideMobileMenu();
+})
